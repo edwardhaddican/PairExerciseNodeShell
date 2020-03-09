e86e61291620662ee09c771705c9d36adf07944f
Eddie Haddican <edwardhaddican@gmail.com>
2020-03-09 13:13:37 -0400

    removed console logs

--- bash.js
+++ bash.js
@@ -15,10 +15,7 @@ process.stdin.on('data', (data) => {
   const cmd = inputString[0]
   const args = inputString.slice(1)
 
-  console.log("args 0", args[0])
-
   if(cmd === "cat"){
-    console.log("args 0 in if statement", args[0])
     cat("./" + args[0])
   }
 
--- cat.js
+++ cat.js
@@ -8,22 +8,11 @@ const fs = require('fs');
 
  function catFunction (fileName){
 
-  console.log("consolelog of file name", fileName)
-
   fs.readFile(fileName, 'utf8', (err, data) => {
-    console.log("hello in cat function")
       if (err) throw err;
       console.log(data);
     });
 
  }
-
-
-
-
-
-
-
-
 
 module.exports = catFunction
